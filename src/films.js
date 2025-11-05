f9f27f68e6a3ca8471cb25c6fe06b72a08e9e790
blackfel666 <fv10644@gmail.com>
2025-11-05 23:09:52 +0100

    Add the function alldirectors & movies average

--- src/films.js
+++ src/films.js
@@ -1,19 +1,36 @@
 // Exercise 1: Get the array of all directors.
+import {movies} from "./data.js"
+
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map((movies) => { return movies.director });
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
+getAllDirectors(movies)
+
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let sameDirector = array.filter(movies => movies.director === director)
+  return sameDirector;
 }
+
+const peliculasDeSpielberg = getMoviesFromDirector(movies, 'Steven Spielberg');
+console.log(peliculasDeSpielberg);
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let directorMovies = getMoviesFromDirector(array, director)
+  let scoreDirector = directorMovies.reduce((acc , movies) => acc + movies.score, 0)
+  return scoreDirector
 }
+
+const directorBuscado = 'Steven Spielberg';
+
+const promedioFinal = moviesAverageOfDirector(movies, directorBuscado);
+
+console.log(`EXERCICE 3 -> El puntaje promedio de ${directorBuscado} es: ${promedioFinal}`);
+
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
